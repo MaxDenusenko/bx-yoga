cc5872d16dedf279d9d6c3a229728dcc21889362
Nezox <maxdenusenko@gmail.com>
2020-01-24 00:57:57 +0200

    add footer links

--- .bottom.menu.php
+++ .bottom.menu.php
@@ -2,42 +2,42 @@
 $aMenuLinks = Array(
 	Array(
 		"О нас", 
-		"", 
+		"/about/", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
 		"Преподаватели", 
-		"", 
+		"/teachers/", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
 		"Галерея", 
-		"", 
+		"/gallery/", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
 		"Услуги", 
-		"", 
+		"/uslugi/", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
 		"Расписание", 
-		"", 
+		"/raspisanie/", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
 		"Новости", 
-		"", 
+		"/news/", 
 		Array(), 
 		Array(), 
 		"" 
